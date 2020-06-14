02d9aaa954460b64739c2e18e53fb62042eefd05
biddutpaul02 <you@example.com>
2020-06-14 21:20:39 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,17 @@
+(function($){
+    "use strict";
+    jQuery().ready(function($){
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            nav:true;
+            autoplay: false;
+            loop:true;
+            dots:false;
+        })
+
+    
+    });
+    jQuery(window).load(function(){
+        
+    });
+}(jQuery));
